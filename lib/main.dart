4e2854d6b04d8bd348b33dd05c5dd7a60d6ef5e6
Deepsavani2904 <140231447+Deepsavani2904@users.noreply.github.com>
2023-08-01 13:12:24 +0530

    calculator

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 
 
-
-
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
